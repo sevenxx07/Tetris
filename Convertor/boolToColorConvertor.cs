8ce5aa33eecd0a931a920f21574fc08c1fa3d60d
Sára Veselá <104825252+sevenxx07@users.noreply.github.com>
2024-08-09 17:02:49 +0200

    Add files via upload

---
+++ Convertor/boolToColorConvertor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tetris.Convertor
+{
+    public class boolToColorConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool color = (bool)value;
+            if (color)
+            {
+                return "LightSkyBlue";
+            }
+            else
+            {
+                return "DarkBlue";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Convertor/intToColorConvertor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tetris.Convertor
+{
+    public class intToColorConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int color = (int)value;
+            
+            if (color == 0 && parameter == null)
+            {
+                return "White";
+            }
+            else if(color == 1 && parameter == null)
+            {
+                return "DarkBlue";
+            }else if(color == 0) 
+            {
+                return "Black";
+            }
+            else
+            {
+                return "White";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
